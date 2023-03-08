446b84d73b1d26eccc0b0691efb4eaef158ffb57
DarkBenve <cosimo.benvenuti10@gmail.com>
2023-03-08 16:23:38 +0100

    Update_DebugSizeShip

--- Assets/Scripts/Ship/Ship.cs
+++ Assets/Scripts/Ship/Ship.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using UnityEngine;
 
 namespace BattleShip
@@ -17,9 +18,30 @@ namespace BattleShip
         [Range(1, 3)][SerializeField] private int sizeShip;
 
         private ShipData _shipData;
+        private Vector3 _transformLocalScale;
         private void Start()
         {
             _shipData = new ShipData(sizeShip);
+        }
+
+        private void Update()
+        {
+            ShipCreate();
+        }
+
+        private void ShipCreate()
+        {
+            switch (sizeShip) {
+                case 1:
+                    Debug.Log("Grandezza 1-Tile");
+                    break;
+                case 2:
+                    Debug.Log("Grandezza 2-Tile");
+                    break;
+                case 3:
+                    Debug.Log("Grandezza 3-Tile");
+                    break;
+            }
         }
     }
 }
