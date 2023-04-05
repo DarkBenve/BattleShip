498510a5168787eb9c1e866cd2f2e9dd50d52f70
DarkBenve <cosimo.benvenuti10@gmail.com>
2023-04-05 13:25:16 +0200

    Add_TurretAsset

---
+++ Assets/Scripts/Turret/Turret.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace BattleShip
+{
+    public class Turret : MonoBehaviour
+    {
+        [SerializeField] private GameObject bullet;
+        [SerializeField] private Transform spawnBullet;
+
+
+        public IEnumerator ShootTurret(GameObject target)
+        {
+            Instantiate(bullet, spawnBullet);
+            // instantiateBullet.MovePosition(target.transform.position);
+            yield return new WaitForSeconds(10f);
+        }
+    }
+}
